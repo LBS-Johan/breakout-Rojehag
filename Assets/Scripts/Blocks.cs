7bb808fdbde53ee00e372efa9bde02ab40a90ee3
Rojehag <jenz.rojehag@icloud.com>
2024-12-11 11:20:33 +0100

    jobbar på scoreboard

--- Assets/Scripts/Blocks.cs
+++ Assets/Scripts/Blocks.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Blocks : MonoBehaviour
 {
+    
     static int score;
-   
+    [SerializeField]
+    public static TextMeshProUGUI scoreText;
+
+    void Start()
+    {
+        score = 0;
+        scoreText = GameObject.Find()
+    }
+
     private void OnCollisionEnter2D(Collision2D _collision)
     {
         score += 1;
+        scoreText.text = "Score: " + score;
 
         Destroy(gameObject);
     }
